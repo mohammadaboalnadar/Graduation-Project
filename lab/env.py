be53f89d5ba13d4b8aa05ab560b974a19087b7d1
TheFortex <airtex.mc@gmail.com>
2026-05-06 20:26:30 +0300

    v9.1s2.0 -> random initial pv noise + varying target height

--- lab/env.py
+++ lab/env.py
@@ -86,8 +86,8 @@ class UnitreeA1Env(gym.Env):
 		super().reset(seed=seed)
 
 		mujoco.mj_resetDataKeyframe(self.model, self.data, 0)
-		# self.data.qpos[:] += self.np_random.uniform(-0.01, 0.01, self.model.nq)
-		# self.data.qvel[:] += self.np_random.uniform(-0.01, 0.01, self.model.nv)
+		self.data.qpos[:] += self.np_random.uniform(-0.01, 0.01, self.model.nq)
+		self.data.qvel[:] += self.np_random.uniform(-0.01, 0.01, self.model.nv)
 		# Randomise starting yaw so the robot learns from any orientation
 		# start_yaw = self.np_random.uniform(-np.pi, np.pi)
 		# cy, sy = np.cos(start_yaw / 2), np.sin(start_yaw / 2)
@@ -107,7 +107,7 @@ class UnitreeA1Env(gym.Env):
 		# self.target_pitch = self.np_random.uniform(-0.3, 0.3)  # ~±17 degrees
 		# self.target_roll  = self.np_random.uniform(-0.2, 0.2)  # ~±11 degrees
 		# self.target_quat  = self._quat_from_yaw_pitch_roll(self.target_yaw, self.target_pitch, self.target_roll)
-		# self.target_height = self.np_random.uniform(0.15, 0.35)
+		self.target_height = self.np_random.uniform(0.15, 0.35)
 
 		return self._get_obs(), {}
 
@@ -254,7 +254,7 @@ class UnitreeA1Env(gym.Env):
 		base_height  = self.data.qpos[2]
 		base_quat    = self.data.qpos[3:7]
 		uprightness  = base_quat[0] ** 2
-		return bool(base_height < 0.15 or uprightness < 0.5)
+		return bool(base_height < 0.12 or uprightness < 0.5)
 
 	# ──────────────────────────────────────────────────────────────────
 	def render(self):
--- lab/train.py
+++ lab/train.py
@@ -25,7 +25,7 @@ dt = float(model.opt.timestep)
 
 #[OPTIONS]:
 
-VERSION = "9.1"
+VERSION = "9.1s2.0"
 TOTAL_TIMESTEPS = 100_000_000
 CHECKPOINT_FREQ = 1_000_000  # Save a checkpoint every N timesteps
 MAX_EPISODE_STEPS = 4*50 # N seconds at 50Hz
@@ -132,10 +132,10 @@ if __name__ == "__main__":
 		VecNormalize.load(f"{modelsPath}/a1_walk_v{VERSION}_vecnormalize.pkl", env)
 		model = PPO.load(f"{modelsPath}/a1_walk_v{VERSION}.zip", env=env)
 
-		model.learning_rate = get_linear_fn(3e-4, 1e-5, 1)
-		model.clip_range    = get_linear_fn(0.2,  0.05, 1)
+		# model.learning_rate = get_linear_fn(3e-4, 1e-5, 1)
+		# model.clip_range    = get_linear_fn(0.2,  0.2, 1)
 		# model.learning_rate = float(5e-5)
-		model.target_kl     = 0.05
+		# model.target_kl     = None
 		# model.ent_coef	  = 0.005
 	else:
 		print("Creating new model")
